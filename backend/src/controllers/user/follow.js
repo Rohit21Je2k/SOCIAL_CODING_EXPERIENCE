f0136f65236021c520b1ab1eb9bc6e71b974c1dc
abhishek <abhiabhii2000@gmail.com>
2022-04-21 00:13:51 +0530

    userCheck validated

--- backend/src/controllers/user/follow.js
+++ backend/src/controllers/user/follow.js
@@ -1,10 +1,17 @@
 import usermodel from "../../models/user.js";
-
+import { httpError } from "../../util/functions/_index.js";
 const follow = async (req, res, next) => {
   const username1 = req.decodedToken.username;
   const username2 = req.body.username2;
 
   try {
+    if(!username1 || username1!=''){
+      throw httpError("user name 1 is invalid!");
+    }
+    if(!username2 || username2!=''){
+      throw httpError("user name 2 is invalid!");
+    }
+
     const user1 = await usermodel.findOne({ username: username1 });
     const user2 = await usermodel.findOne({ username: username2 });
     if (user1.following.indexOf(username2) == -1) {
